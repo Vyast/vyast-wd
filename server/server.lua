eb1088058a84cd3e2d69eaed6eefda307a33c655
Vyast <67253117+Vyast@users.noreply.github.com>
2022-04-01 04:04:01 -0600

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,15 +1,10 @@
-if config.kick then
-    RegisterNetEvent('vyast-weaponDetection:server:kick-player', function(bool)
-        local src = source
+if config?.kick then
+	RegisterNetEvent('vyast-wd:kick', function(bool)
+		local reason = config?.kickReasons?.ammo
+		if bool then reason = config?.kickReasons?.damage end
 
-        local reason = config.kickReasons.ammo
-
-        if bool then
-            reason = config.kickReasons.damage
-        end
-
-        print(string.format('^4[vyast-weaponDetection]^0 - [%s] %s has been kicked for reason: %s', src, GetPlayerName(src), reason))
-        
-        DropPlayer(src, string.format('You have been kicked.\n\nReason: %s', reason))
-    end)
+		print(('^4[%s] %s has been kicked for reason:^7 %s'):format(source, GetPlayerName(source), reason))
+		
+		DropPlayer(source, ('You have been kicked.\n\nReason: %s'):format(reason))
+	end)
 end
